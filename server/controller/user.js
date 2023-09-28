2c8d93ea5f05618a7ec7c51d001e769f5ed793ae
phucnguyxn <phucnguyen10072001@gmail.com>
2023-09-28 09:11:16 +0700

    CRUD

--- server/controller/user.js
+++ server/controller/user.js
@@ -38,15 +38,15 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if (response && await response.isCorrectPassword(password)) {
         //Tach pw va role khoi response
-        const { password, role, ...userData } = response.toObject()
+        const { password, role, refreshToken, ...userData } = response.toObject()
         //Tao accessToken
         const accessToken = generateAccessToken(response._id, role)
         //Tao refreshToken
-        const refreshToken = generateRefreshToken(response._id)
+        const newRefreshToken = generateRefreshToken(response._id)
         //Luu refreshToken vao dtb
-        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
+        await User.findByIdAndUpdate(response._id, { refreshToken: newRefreshToken }, { new: true })
         //Luu refreshToken vao cookie
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        res.cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
         return res.status(200).json(({
             success: true,
             accessToken,
@@ -61,7 +61,7 @@ const getCurrent = asyncHandler(async (req, res) => {
     const { _id } = req.user
     const user = await User.findById(_id).select('-refreshToken -password -role')
     return res.status(200).json({
-        success: false,
+        success: user ? true : false,
         rs: user ? user : 'User not found'
     })
 })
@@ -137,7 +137,44 @@ const resetPassword = asyncHandler(async (req, res) => {
         mes: user ? 'Update password' : 'Something went wrong'
     })
 })
+const getUsers = asyncHandler(async (req, res) => {
+    const response = await User.find().select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        users: response
+    })
+})
+const deleteUser = asyncHandler(async (req, res) => {
+    const { _id } = req.query
+    if (!_id) throw new Error('Missing inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedUser: response ? `User with email ${response.email} delete` : 'No user delete'
+    })
+})
 
+const updateUser = asyncHandler(async (req, res) => {
+    // 
+    const { _id } = req.user
+    if (!_id || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Some thing went wrong'
+    })
+})
+
+const updateUserByAdmin = asyncHandler(async (req, res) => {
+    // 
+    const { uid } = req.params
+    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(uid, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Some thing went wrong'
+    })
+})
 module.exports = {
     register,
     login,
@@ -145,5 +182,9 @@ module.exports = {
     refreshAccessToken,
     logout,
     forgotPassword,
-    resetPassword
+    resetPassword,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require('./user')
+const productRouter = require('./product')
 const { notFound, errHandler } = require('../middlewares/errorhandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
+    app.use('/api/product', productRouter)
 
 
 
